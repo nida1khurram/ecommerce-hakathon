0a85b714cbd33ca919df282e13e6c090974f4d3f
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-12-05 14:28:52 +0500

    footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -14,13 +14,13 @@ export default function Footer() {
    <>
    <div className={`w-full  h-auto md:h-[440px] bg-[#000] text-white `}>
     {/* sub div */}
-    <div className={`${poppins.className} w-full md:w-[1170px] h-auto md:h-[236px] bg-[#000] grid md:grid-cols-5 mx-auto items-center border-2 border-white `} >
+    <div className={`${poppins.className} w-full md:w-[1170px] h-auto md:h-[236px] bg-[#000] grid md:grid-cols-5 mx-auto items-center `} >
  {/* 1 */}
-    <div className='md:w-[217px] md:h-[188px]'>
-        <h1  className={`${inter.className} text-2xl font-bold `}>Exclusive</h1>
-        <p className={`text-xl`}>Subscribe</p>
-        <p className={`text-[#fafafa]`}>Get 10% off your first order</p>
-        <div className='w-[217px] h-[48px] border-2 border-[#fafafa] rounded-sm flex'>
+    <div className='md:w-[217px] md:h-[188px] mt-20'>
+        <h1  className={`${inter.className} text-2xl font-bold mb-4`}>Exclusive</h1>
+        <p className={`text-xl mb-4`}>Subscribe</p>
+        <p className={`text-[#fafafa] mb-4`}>Get 10% off your first order</p>
+        <div className='w-[217px] h-[48px] rounded-sm flex '>
             <input
           type="text"
           placeholder="Enter your email"
@@ -37,11 +37,11 @@ export default function Footer() {
     </div>
     {/* 2 */}
   
-    <div className='md:w-[175px] md:h-[180px]'>
-        <h1  className={` text-xl font-bold `}>Support</h1>
-        <p className={`text-[16px] text-[#fafafa]`}>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh</p>
+    <div className='md:w-[175px] md:h-[180px] mt-20'>
+        <h1  className={` text-xl font-bold  mb-4`}>Support</h1>
+        <p className={`text-[16px] text-[#fafafa] mb-4`}>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh</p>
         <Link href={"/"}>
-        <p className={`text-[16px] text-[#fafafa]`}>exclusive@gmail.com</p>
+        <p className={`text-[16px] text-[#fafafa] mb-4`}>exclusive@gmail.com</p>
               </Link>
         <Link href={"/"}>
         <p className={`text-[16px] text-[#fafafa]`}>+88015-88888-9999</p>
@@ -49,24 +49,24 @@ export default function Footer() {
         </div>
     
     {/* 3 */}
-    <div className='md:w-[123px] md:h-[236px]'>
-    <h1  className={` text-xl font-bold `}>Account</h1>
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>My Account</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Login / Register</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Cart</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Wishlist</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Shop</p></Link> 
+    <div className='md:w-[123px] md:h-[236px] mt-28'>
+    <h1  className={` text-xl font-bold mb-4 `}>Account</h1>
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>My Account</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Login / Register</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Cart</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Wishlist</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Shop</p></Link> 
     </div>
     {/* 4 */}
-    <div className='md:w-[109px] md:h-[196px]'>
-    <h1  className={` text-xl font-bold `}> Quick Link</h1>
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Privacy Policy</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Terms Of Use</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>FAQ</p></Link> 
-    <Link href={"/"}><p className={`text-[16px] text-[#fafafa]`}>Contact</p></Link> 
+    <div className='md:w-[109px] md:h-[196px] mt-20'>
+    <h1  className={` text-xl font-bold  mb-4`}> Quick Link</h1>
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Privacy Policy</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Terms Of Use</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>FAQ</p></Link> 
+    <Link href={"/"}><p className={`text-[16px] text-[#fafafa] mb-4`}>Contact</p></Link> 
     </div>
     {/* 5 */}
-    <div className='w-[198px] h-[210px]'>
+    <div className='w-[198px] h-[210px] mt-20'>
     <h1  className={` text-xl font-bold `}>Download App</h1>
     <Link href={"/"}><p className={`text-[12px] text-[#fafafa]`}>Save $3 with App New User Only</p></Link> 
     {/* pic div */}
